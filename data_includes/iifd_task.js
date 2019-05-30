6f84d8a254e8f5ac2143e1d5bb3f063da246521f
Rachel Dudley <rachel.elaine.dudley@gmail.com>
2019-05-30 11:50:50 +0200

    Update iifd_task.js

--- data_includes/iifd_task.js
+++ data_includes/iifd_task.js
@@ -5,9 +5,9 @@
 
 PennController.ResetPrefix(null);
 
-PennController.PreloadZip("https://web-risc.ens.fr/~rdudley/public_html/iifd/judgment/cards/zipped.zip");
+//PennController.PreloadZip("https://web-risc.ens.fr/~rdudley/public_html/iifd/judgment/cards/zipped.zip");
 
-//PennController.AddHost("http://web-risc.ens.fr/~rdudley/iifd/judgment/");
+PennController.AddHost("http://web-risc.ens.fr/~rdudley/iifd/judgment/");
           //ACTION: make sure files can be acquired in zipped version
           //ACTION: check that results file prints fine
 PennController.Sequence( "welcome", "intro1", "intro2", "intro3","intro4","intro5", "intro6","intro8", "intro9", "intro10","intro7", "practice1", "practice2", "practice3", "practice4", "practice5", "practice6", "intro11",
